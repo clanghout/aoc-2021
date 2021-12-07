c644655fd9b665ebc25f8ac60ee726924f275b94
clanghout <chris.langhout@gmail.com>
2021-12-07 13:56:12 +0100

    Improve day 7

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -25,8 +25,8 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn parse_input(contents: &str) -> Vec<usize> {
     contents
         .lines()
-        .filter(|x| !x.is_empty())
-        .next().unwrap()
+        .find(|x| !x.is_empty())
+        .unwrap()
         .split(",")
         .map(|x| {
             x.parse().unwrap()
@@ -34,28 +34,35 @@ fn parse_input(contents: &str) -> Vec<usize> {
 }
 
 fn calc_part1(crabs: &[usize]) -> usize {
-    crabs.iter().enumerate().map(|(i, _)| {
-        crabs
-            .iter()
-            .enumerate()
-            .fold(0, |acc, (j, crab2)| acc + crab2 * (i.max(j) - i.min(j)))
-    }).min().unwrap()
+    crabs
+        .iter()
+        .enumerate()
+        .map(|(i, _)| {
+            crabs
+                .iter()
+                .enumerate()
+                .fold(0, |acc, (j, crab2)| acc + crab2 * (i.max(j) - i.min(j)))
+        })
+        .min()
+        .unwrap()
 }
 
 fn calc_part2(crabs: &[usize]) -> usize {
-    crabs.iter().enumerate().fold(usize::MAX, |acc, (i, _)| {
-        let cost_pos_1 = crabs
-            .iter()
-            .enumerate()
-            .fold(0, |acc, (j, crab2)| acc + part2_fuel_cost(i, j, crab2));
-        if acc < cost_pos_1 { acc } else {
-            cost_pos_1
-        }
-    })
+    crabs
+        .iter()
+        .enumerate()
+        .map(|(i, _)| {
+            crabs
+                .iter()
+                .enumerate()
+                .fold(0, |acc, (j, crab2)| acc + part2_fuel_cost(i, j, *crab2))
+        })
+        .min()
+        .unwrap()
 }
 
 // Calculate the fuel cost from pos i to j for x amount of crabs
-fn part2_fuel_cost(i: usize, j: usize, amount_of_crabs: &usize) -> usize {
+fn part2_fuel_cost(i: usize, j: usize, amount_of_crabs: usize) -> usize {
     let distance = i.max(j) - i.min(j) + 1;
     if distance == 0 { 0 } else {
         distance * (distance - 1) / 2 * amount_of_crabs
@@ -67,7 +74,7 @@ fn test() {
     let test = r#"16,1,2,0,4,2,7,1,2,14"#;
 
     let vec = parse_input(test);
-    assert_eq!(part2_fuel_cost(16, 5, &1), 66);
+    assert_eq!(part2_fuel_cost(16, 5, 1), 66);
     assert_eq!(calc_part1(&vec), 37);
     assert_eq!(calc_part2(&vec), 168);
 }
